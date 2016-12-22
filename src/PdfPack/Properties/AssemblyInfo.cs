59c6e3294f5fea3f440fbd4280972cca39dec37d
yoshiro.yano <yoshiro.yano@findex.co.jp>
2016-12-22 11:08:44 +0900

    サマリ用XMLファイルの出力設定を有効化 アセンブリの説明を追加

--- src/PdfPack/Properties/AssemblyInfo.cs
+++ src/PdfPack/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("PdfPack")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("iTextShaprおよびPdfiumViewerの結合ライブラリ")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PdfPack")]
